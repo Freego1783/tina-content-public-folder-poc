5c3f1cb9dd1355fd8d77f266e268f88942cf703d
Hugo Pernet <hugopernet@ssw.fr>
2025-06-12 17:23:15 +0200

    updating next config for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.tina.io',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+      }
+    ],
+  },
   async rewrites() {
     return [
       {
