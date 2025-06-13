687dad485cfff7173ef29ddcbaa99352872448e0
Hugo Pernet <hugopernet@ssw.fr>
2025-06-13 16:03:10 +0200

    making rules pages static

--- app/rules/[...filename]/page.tsx
+++ app/rules/[...filename]/page.tsx
@@ -1,18 +1,24 @@
 import RuleClientPage from "./client-page";
 import client from "../../../tina/__generated__/client";
 
+
+export async function generateStaticParams() {
+  const rules = await client.queries.ruleConnection();
+  const paths =
+    rules.data?.ruleConnection.edges?.map((page) => ({
+        filename: [page?.node?._sys.relativePath.split("/")[0]],
+      })) || [];
+  return paths;
+}
+
 export default async function RulePage({
   params,
 }: {
   params: { filename: string[] };
 }) {
-
-
   const data = await client.queries.rule({
     relativePath: `${params.filename}/rule.md`,
   });
 
-  return (
-    <RuleClientPage {...data}></RuleClientPage>
-  );
+  return <RuleClientPage {...data}></RuleClientPage>;
 }
